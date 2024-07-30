d8097e2e753582d2997208c9f943cbeaddb64c27
webKanchan1234 <kanchankr15153795@gmail.com>
2024-07-30 22:30:27 +0530

    build

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const bodyParser=require("body-parser")
 const errorMiddleware = require("./middleware/error")
-
+const path = require("path");
 
 dotenv.config({path:"backend/config/config.env"})
 // databaseConnection()
@@ -28,6 +28,12 @@ app.use("/api/v1",user)
 //         message:error.message
 //     })
 // })
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
 app.use(errorMiddleware)
 
 module.exports = app
