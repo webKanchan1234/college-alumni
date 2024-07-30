fdb70d2f811d647213d2d6c10055efa15171158b
webKanchan1234 <kanchankr15153795@gmail.com>
2024-07-30 22:46:16 +0530

    build

--- backend/config/databaseConnection.js
+++ backend/config/databaseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const databaseConnection = () => {
   mongoose
-    .connect("mongodb://localhost:27017/college")
+    .connect("mongodb+srv://kanchankr15153795:arQnumUzQjXnweev@cluster0.dky0fin.mongodb.net/college?retryWrites=true&w=majority")
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
     });
